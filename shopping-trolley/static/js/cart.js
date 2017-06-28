26c268a647f334f3c32c7fddd7067fd148ea3b50
hhz <860601963@qq.com>
2017-06-28 16:50:46 +0800

    add something in shopping-trolley

--- shopping-trolley/static/js/cart.js
+++ shopping-trolley/static/js/cart.js
@@ -1,21 +1,25 @@
-var vm = new Vue({
+new Vue({
     el:"#app",
     data:{
         totalMoney:0,
         productList: []
     },
     filters:{
-
+        formatMoney: function (value) {
+            return "￥ " + value.toFixed(2);
+        }
     },
     mounted: function () {
-        this.cartView();
+        this.$nextTick(function (){
+            this.cartView();
+        });
     },
     methods:{
         cartView: function () {
-            var _this = this;
-            this.$http.get("/static/data/cartData.json").then(function (res) {
-                _this.productList = res.data.result.list;
-                _this.totalMoney = res.data.result.totalMoney;
+            let _this = this;
+            this.$http.get("/static/data/cartData.json").then((res) => {
+                this.productList = res.data.result.list;
+                this.totalMoney = res.data.result.totalMoney;
             });
         }
     }
